6c2bf988c086e6154806ec8fae68ffa9c91bac4e
wande <yewandeadeyemi84@gmail.com>
2023-11-24 13:05:48 +0100

    updated

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { images } from '../../constants';
 
+import { SubHeading } from '../../components';
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+<div className='app__wrapper_info'>
+<SubHeading title='Contact'/>
+<h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}>Find Us</h1>
+<div className='app__wrapper_contact'>
+<p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+<p className='p__cormorant' style={{color: '#dccca87', margin: '2rem 0'}}>Opening Hours</p>
+<p className='p__opensans' style={{marginTop: '2rem'}}>Mon - Fri: 10:00 am - 02:00 am</p>
+<p className='p__opensans' style={{marginTop: '2rem'}}>Sat - Sun: 10:00 am - 03:00 am</p>
+<button type='button' className='custom__button' style={{marginTop: '2rem'}}>View More</button>
+</div>
+</div>
+<div className='app__wrapper_img'>
+  <img src={images.findus} alt="" />
+
+</div>
   </div>
 );
 
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { images, data } from '../../constants';
 
+import { SubHeading } from '../../components';
 import './Laurels.css';
 
+
+
+const AwardCard = ({award: {imgUrl, title, subtitle}}) => {
+
+
+return(
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="" />
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{color: '#DCCA87'}}>{title}</p>
+      <p className='p__cormorant' >{subtitle}</p>
+
+    </div>
+  </div>
+)
+  
+}
+
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper flex__center section__padding' id='awards'>
+   <div className='app_wrapper_info'>
+<SubHeading title='Awards & recognition'/>
+<h1 className='headtext__cormorant'>Our Laurels</h1>
+<div className='app__laurels_awards'>
+{data.awards.map((award) => <AwardCard award={award} key={award.title}/>
+)}
+</div>
+   </div>
+   <div className='app_wrapper_img'>
+<img src={images.laurels} alt="" className='img_award' />
+</div>
   </div>
 );
 
