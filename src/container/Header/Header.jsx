c2b76b724b7aed42314a30cbf164cb988de7c5a0
wande <yewandeadeyemi84@gmail.com>
2023-11-24 01:43:51 +0100

    updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => (
   <SubHeading title="Chase The new flavour"/>
   <h1 className='app__header-h1'>The key to Fine dining</h1>
   <p className='p__opensans' style={{margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+  <button type='button' className='custom__button'>Explore More</button>
     </div>
     <div className='app__wrapper_img'>
 <img src={images.welcome} alt="header img" />
